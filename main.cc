96ca4d6bd1a2c7b6556a24e3d54b87e9f7cc08b4
darndt <daniel@rndt.co>
2018-11-01 17:02:21 +0100

    task #9

--- main.cc
+++ main.cc
@@ -2,7 +2,6 @@
 #include "dict.hh"
 #include "parameter.hh"
 #include "ui.hh"
-#include <iostream>
 
 int main(int argc, char* argv[]) {
     UI ui;
@@ -11,9 +10,14 @@ int main(int argc, char* argv[]) {
     ui.output(p);
     Dict dict(p.use_dict(), p.get_dict_filename(), p.should_print_index());
 
-    auto input = p.has_file() ? ui.read_file(p.get_filename()) : ui.read_input();
-    auto statistics = bl.run(input);
-    dict.set_tokens(statistics.get_unique());
-    ui.output(statistics, p.should_print_index());
-    ui.output(dict);
+    bool next = false;
+    do {
+        auto input =
+            p.has_file() ? ui.read_file(p.get_filename()) : ui.read_input();
+        auto statistics = bl.run(input);
+        dict.set_tokens(statistics.get_unique());
+        ui.output(statistics, p.should_print_index());
+        ui.output(dict);
+        next = statistics.get_token_count() > 0;
+    } while (!p.has_file() && next);
 }
